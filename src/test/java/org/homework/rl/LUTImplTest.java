9cfdddf002a7334b764386e9d327030b369fe622
michaelma <taomaqueens@gmail.com>
2022-10-25 16:03:06 -0700

    RL implement

--- src/test/java/org/homework/rl/LUTImplTest.java
+++ src/test/java/org/homework/rl/LUTImplTest.java
@@ -1,9 +1,19 @@
 package org.homework.rl;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class LUTImplTest {
 
   @BeforeEach
   void setUp() {}
+
+  @Test
+  void TEST_INITIALIZE_LUT() {
+    final LUTImpl lut = new LUTImpl(10, 3, 3, 2, .5, new int[] {1, 2}, new int[] {1, 2});
+
+    assertArrayEquals(lut.getQTable().shape(), new long[] {3, 3, 2});
+  }
 }
