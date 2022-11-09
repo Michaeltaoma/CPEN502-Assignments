a8b9cf7d192244a3f17c0c588a16e8f967d316c2
michaelma <taomaqueens@gmail.com>
2022-11-08 17:17:00 -0800

    Enable logging to file in robot, previous slf4j method was disabled by robocode

---
+++ src/main/java/org/homework/Util.java
@@ -0,0 +1,22 @@
+package org.homework;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Util {
+
+    /**
+     * Close output stream
+     *
+     * @param fileOutputStream output stream created
+     */
+    public static void closeOutputStream(final OutputStream fileOutputStream) {
+        if (fileOutputStream == null) return;
+
+        try {
+            fileOutputStream.close();
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
