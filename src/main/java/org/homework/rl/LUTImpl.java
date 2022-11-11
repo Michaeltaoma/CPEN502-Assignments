08ddd32176d3aec6483e8824bfe218ae461d17d3
Estelle Qiu <44105167+xqiu1@users.noreply.github.com>
2022-11-10 16:02:21 -0800

    finish choose greedy action

--- src/main/java/org/homework/rl/LUTImpl.java
+++ src/main/java/org/homework/rl/LUTImpl.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.NDArrayIndex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,30 +17,28 @@ public class LUTImpl implements LUTInterface {
 
     private static final Logger logger = LoggerFactory.getLogger(LUTImpl.class);
     private static final DataType LUT_DATA_TYPE = DataType.DOUBLE;
-    private final int argNumInputs;
-    private final long battleFieldWidth;
-    private final long battleFieldHeight;
-    private final double epsilon;
+
+    private final int myHPTypes;
+    private final int enemyHPTypes;
+    private final int distanceToEnemyTypes;
+    private final int distanceToWallTypes;
     private final int actionSize;
-    private final int[] argVariableFloor;
-    private final int[] argVariableCeiling;
+    private final double epsilon;
     public INDArray qTable;
 
     public LUTImpl(
-            final int argNumInputs,
-            final long battleFieldWidth,
-            final long battleFieldHeight,
+            final int myHPTypes,
+            final int enemyHPTypes,
+            final int distanceToEnemyTypes,
+            final int distanceToWallTypes,
             final int actionSize,
-            final double epsilon,
-            final int[] argVariableFloor,
-            final int[] argVariableCeiling) {
-        this.argNumInputs = argNumInputs;
-        this.battleFieldWidth = battleFieldWidth;
-        this.battleFieldHeight = battleFieldHeight;
+            final double epsilon) {
+        this.myHPTypes = myHPTypes;
+        this.enemyHPTypes = enemyHPTypes;
+        this.distanceToEnemyTypes = distanceToEnemyTypes;
+        this.distanceToWallTypes = distanceToWallTypes;
         this.actionSize = actionSize;
         this.epsilon = epsilon;
-        this.argVariableFloor = argVariableFloor;
-        this.argVariableCeiling = argVariableCeiling;
 
         this.initialiseLUT();
     }
@@ -49,8 +48,10 @@ public class LUTImpl implements LUTInterface {
         this.qTable =
                 Nd4j.rand(
                         LUT_DATA_TYPE,
-                        this.battleFieldHeight,
-                        this.battleFieldHeight,
+                        this.myHPTypes,
+                        this.enemyHPTypes,
+                        this.distanceToEnemyTypes,
+                        this.distanceToWallTypes,
                         this.actionSize);
     }
 
@@ -60,7 +61,7 @@ public class LUTImpl implements LUTInterface {
     }
 
     /** Perform q learning on state action table */
-    int chooseAction(final int... dimension) {
+    public int chooseAction(final int[] dimension) {
         if (Math.random() < this.epsilon) {
             // explore
             return this.chooseRandomAction();
@@ -69,23 +70,27 @@ public class LUTImpl implements LUTInterface {
         return this.chooseGreedyAction(dimension);
     }
 
-    int chooseGreedyAction(final int... dimension) {
+    int chooseGreedyAction(final int[] dimension) {
         // given state, get the max action value from q table
-        // example: action = np.argmax(Q_table[state[0], state[1],state[2],state[3]])
+        // example: action = np.argmax(Q_table[state[0], state[1],state[2],...])
         // -> find the best action based on Q(s,a') from Q(s)
         // so dimension should contain state info
-        return 0;
+        INDArray qValues = this.qTable.get(NDArrayIndex.point(dimension[0]));
+        for (int i = 1; i < dimension.length; ++i) {
+            qValues = qValues.get(NDArrayIndex.point(dimension[i]));
+        }
+        return qValues.argMax().getInt();
     }
 
     int chooseRandomAction() {
         return new Random().nextInt(this.actionSize);
     }
 
-    void setQValue(final double qValue, final int... dimension) {
+    void setQValue(final double qValue, final int[] dimension) {
         this.qTable.putScalar(dimension, qValue);
     }
 
-    double getQValue(final int... dimension) {
+    double getQValue(final int[] dimension) {
         return this.qTable.getDouble(dimension);
     }
 
