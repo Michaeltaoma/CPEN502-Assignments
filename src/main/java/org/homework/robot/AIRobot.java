0d25c4f64982d4c8a162c50bef7c3cb76f173e64
michaelma <taomaqueens@gmail.com>
2022-11-13 12:13:56 -0800

    implement fire action

--- src/main/java/org/homework/robot/AIRobot.java
+++ src/main/java/org/homework/robot/AIRobot.java
@@ -9,12 +9,15 @@ import org.homework.robot.model.StateName;
 import robocode.AdvancedRobot;
 import robocode.BattleEndedEvent;
 import robocode.DeathEvent;
+import robocode.HitRobotEvent;
 import robocode.RobocodeFileOutputStream;
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 
 import static org.homework.Util.closeOutputStream;
@@ -29,6 +32,7 @@ public class AIRobot extends AdvancedRobot {
     private State prevState = this.currentState;
     private LUTImpl lut = new LUTImpl(this.currentState);
     private Action currentAction;
+    private double bearing = 0.0;
 
     @Override
     public void run() {
@@ -58,7 +62,10 @@ public class AIRobot extends AdvancedRobot {
         this.setTurnLeft(this.currentAction.getDirection()[0]);
         this.setTurnRight(this.currentAction.getDirection()[1]);
         this.setAhead(this.currentAction.getDirection()[2]);
-        this.info(String.format("Current Action: %s", this.currentAction.name()));
+        if (this.currentAction == Action.FIRE) {
+            this.setTurnGunRight(this.getHeading() - this.getGunHeading() + this.bearing);
+            this.setFire(this.currentAction.getDirection()[3]);
+        }
         this.execute();
     }
 
@@ -69,6 +76,8 @@ public class AIRobot extends AdvancedRobot {
      * @return State represent current
      */
     public State getCurrentState(final ScannedRobotEvent event) {
+        this.bearing = event.getBearing();
+
         return ImmutableState.builder()
                 .currentHP(StateName.HP.values()[this.toCategoricalState(event.getEnergy())])
                 .currentEnemyHP(
@@ -78,8 +87,25 @@ public class AIRobot extends AdvancedRobot {
                                 .values()[this.toCategoricalState(event.getDistance())])
                 .currentDistanceToWall(
                         StateName.DISTANCE_TO_WALL
-                                .values()[this.toCategoricalState(this.getDistanceRemaining())])
+                                .values()[
+                                this.toCategoricalState(
+                                        this.getDistanceFromWall(this.getX(), this.getY()))])
                 .build();
+    }
+
+    /**
+     * Calculate distance from wall
+     *
+     * @param x1 x1
+     * @param y1
+     * @return
+     */
+    public double getDistanceFromWall(final double x1, final double y1) {
+        final double width = this.getBattleFieldWidth();
+        final double height = this.getBattleFieldHeight();
+        final double disb = height - y1, disl = x1, disr = width - x1;
+
+        return Collections.max(Arrays.asList(y1, disb, disl, disr));
     }
 
     /**
@@ -100,7 +126,6 @@ public class AIRobot extends AdvancedRobot {
     public void onScannedRobot(final ScannedRobotEvent event) {
         this.prevState = this.currentState;
         this.currentState = this.getCurrentState(event);
-        this.info(String.format("Current State: %s\n", this.currentState));
     }
 
     /**
@@ -121,6 +146,17 @@ public class AIRobot extends AdvancedRobot {
     @Override
     public void onWin(final WinEvent event) {
         this.reward += 10 * BASIC_REWARD;
+        this.updateQValue();
+    }
+
+    /**
+     * Call when robot die
+     *
+     * @param event
+     */
+    @Override
+    public void onHitRobot(final HitRobotEvent event) {
+        this.reward -= 4 * BASIC_REWARD;
         this.updateQValue();
     }
 
--- src/main/java/org/homework/robot/model/Action.java
+++ src/main/java/org/homework/robot/model/Action.java
@@ -1,9 +1,10 @@
 package org.homework.robot.model;
 
 public enum Action {
-    TO_LEFT(new double[] {50.0, 0.0, 50.0}),
-    TO_RIGHT(new double[] {0.0, 50.0, 50.0}),
-    AHEAD(new double[] {0.0, 0.0, 50.0});
+    TO_LEFT(new double[] {50.0, 0.0, 50.0, 0.0}),
+    TO_RIGHT(new double[] {0.0, 50.0, 50.0, 0.0}),
+    AHEAD(new double[] {0.0, 0.0, 50.0, 0.0}),
+    FIRE(new double[] {50.0, 0.0, 50.0, 3.0});
 
     final double[] direction;
 
