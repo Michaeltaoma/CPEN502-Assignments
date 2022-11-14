23c465733a925f8e58d85c349ec0871143fc1040
michaelma <taomaqueens@gmail.com>
2022-11-13 19:51:41 -0800

    implement get state from index and refine load

--- src/main/java/org/homework/robot/AIRobot.java
+++ src/main/java/org/homework/robot/AIRobot.java
@@ -6,7 +6,18 @@ import org.homework.robot.model.Action;
 import org.homework.robot.model.ImmutableState;
 import org.homework.robot.model.State;
 import org.homework.robot.model.StateName;
-import robocode.*;
+import robocode.AdvancedRobot;
+import robocode.BattleEndedEvent;
+import robocode.BulletHitEvent;
+import robocode.BulletMissedEvent;
+import robocode.DeathEvent;
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.RobocodeFileOutputStream;
+import robocode.RoundEndedEvent;
+import robocode.ScannedRobotEvent;
+import robocode.WinEvent;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -27,32 +38,20 @@ public class AIRobot extends AdvancedRobot {
     private LUTImpl lut = new LUTImpl(this.currentState);
     private Action currentAction;
     private double bearing = 0.0;
-
     private int winRound = 0;
-
     private int totalRound = 0;
 
     @Override
     public void run() {
         this.initRobocodeFileOutputStream();
+        this.load();
         while (true) {
             this.setTurnRadarLeftRadians(2 * Math.PI);
-            this.scan();
             this.currentAction = this.chooseCurrentAction();
             this.act();
             this.updateQValue();
             this.reward = .0;
         }
-    }
-
-    /** Update q value */
-    public void updateQValue() {
-        this.lut.computeQValue(
-                this.prevState,
-                this.currentState,
-                this.currentAction.ordinal(),
-                this.reward,
-                this.isOnPolicy);
     }
 
     /** Act based on action */
@@ -92,6 +91,23 @@ public class AIRobot extends AdvancedRobot {
     }
 
     /**
+     * Get Current State when the enemy is not scanned
+     *
+     * @return Current State
+     */
+    public State getCurrentState() {
+        return ImmutableState.builder()
+                .from(this.currentState)
+                .currentHP(StateName.HP.values()[this.toCategoricalState(this.getEnergy())])
+                .currentDistanceToWall(
+                        StateName.DISTANCE_TO_WALL
+                                .values()[
+                                this.toCategoricalState(
+                                        this.getDistanceFromWall(this.getX(), this.getY()))])
+                .build();
+    }
+
+    /**
      * Calculate distance from wall
      *
      * @param x1 x1
@@ -115,15 +131,49 @@ public class AIRobot extends AdvancedRobot {
         return Action.values()[this.lut.chooseAction(this.currentState)];
     }
 
+    /** Update q value */
+    public void updateQValue() {
+        this.lut.computeQValue(
+                this.prevState,
+                this.currentState,
+                this.currentAction.ordinal(),
+                this.reward,
+                this.isOnPolicy);
+    }
+
     /**
-     * Called when the enemy robot has been scanned
+     * Update robot's current state and previous state when the enemy is scanned
      *
-     * @param event
+     * @param event event when a enemy robot is being scanned
      */
-    @Override
-    public void onScannedRobot(final ScannedRobotEvent event) {
+    private void updateRobotState(final ScannedRobotEvent event) {
         this.prevState = this.currentState;
         this.currentState = this.getCurrentState(event);
+    }
+
+    /** Update robot's current state and previous state */
+    private void updateRobotState() {
+        this.prevState = this.currentState;
+        this.currentState = this.getCurrentState();
+    }
+
+    private void updateRound(final boolean isWin) {
+        if (isWin) {
+            this.winRound++;
+        }
+        this.totalRound++;
+    }
+
+    /** Update current robot state and update q value */
+    private void updateLearning() {
+        this.updateRobotState();
+        this.updateQValue();
+    }
+
+    /** Called when the enemy robot has been scanned */
+    @Override
+    public void onScannedRobot(final ScannedRobotEvent event) {
+        this.updateRobotState(event);
     }
 
     /**
@@ -138,6 +188,8 @@ public class AIRobot extends AdvancedRobot {
 
     @Override
     public void onRoundEnded(final RoundEndedEvent event) {
+        this.info(String.format("Entry: %d on end", this.lut.qTable.size()));
+        this.lut.save(this.getDataFile(this.getEntryFileName()));
         closeOutputStream(this.robocodeFileOutputStream);
     }
 
@@ -149,7 +201,7 @@ public class AIRobot extends AdvancedRobot {
     @Override
     public void onWin(final WinEvent event) {
         this.reward += 10 * BASIC_REWARD;
-        this.updateQValue();
+        this.updateLearning();
         this.updateRound(true);
     }
 
@@ -161,6 +213,7 @@ public class AIRobot extends AdvancedRobot {
     @Override
     public void onHitRobot(final HitRobotEvent event) {
         this.reward -= BASIC_REWARD;
+        this.updateLearning();
     }
 
     /**
@@ -169,8 +222,9 @@ public class AIRobot extends AdvancedRobot {
      * @param event
      */
     @Override
-    public void onHitWall(HitWallEvent event) {
+    public void onHitWall(final HitWallEvent event) {
         this.reward -= BASIC_REWARD;
+        this.updateLearning();
     }
 
     /**
@@ -181,6 +235,7 @@ public class AIRobot extends AdvancedRobot {
     @Override
     public void onBulletHit(final BulletHitEvent event) {
         this.reward += 2 * BASIC_REWARD;
+        this.updateLearning();
     }
 
     /**
@@ -190,7 +245,8 @@ public class AIRobot extends AdvancedRobot {
      */
     @Override
     public void onBulletMissed(final BulletMissedEvent event) {
-        this.reward -= .5 * BASIC_REWARD;
+        this.reward -= 2 * BASIC_REWARD;
+        this.updateLearning();
     }
 
     /**
@@ -201,6 +257,7 @@ public class AIRobot extends AdvancedRobot {
     @Override
     public void onHitByBullet(final HitByBulletEvent event) {
         this.reward -= 1 * BASIC_REWARD;
+        this.updateLearning();
     }
 
     /**
@@ -211,7 +268,7 @@ public class AIRobot extends AdvancedRobot {
     @Override
     public void onDeath(final DeathEvent event) {
         this.reward -= 10 * BASIC_REWARD;
-        this.updateQValue();
+        this.updateLearning();
         this.updateRound(false);
     }
 
@@ -234,6 +291,15 @@ public class AIRobot extends AdvancedRobot {
         try {
             this.robocodeFileOutputStream.write(
                     String.format("%s\n", msg).getBytes(StandardCharsets.UTF_8));
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void load() {
+        try {
+            this.lut.load(this.getDataFile(this.getEntryFileName()));
+            this.info(String.format("qtable contains %d entries", this.lut.qTable.size()));
         } catch (final IOException e) {
             throw new RuntimeException(e);
         }
@@ -266,11 +332,12 @@ public class AIRobot extends AdvancedRobot {
         return String.format("robot-%d.txt", new Date().getTime());
     }
 
-    private void updateRound(boolean isWin) {
-        if (isWin) {
-            this.winRound++;
-        }
-        this.totalRound++;
+    /**
+     * Get file that store the weight
+     *
+     * @return filename that store the weight
+     */
+    private String getEntryFileName() {
+        return "AIRobot-crazy-robot.txt";
     }
-
 }
